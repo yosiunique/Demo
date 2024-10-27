d3e8a3add4c13b87d9f8e04fd08f301f6897faa0
Yoseph Getachew <Yoseph.GetachewW@ethio.local>
2024-10-28 00:47:08 +0300

    test

---
+++ src/main/java/com/example/demo/Controller/App.java
@@ -0,0 +1,17 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class App {
+
+    @GetMapping("/test")
+    public String getTest() {
+        return "index";
+    }
+    
+    
+}
