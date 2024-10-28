704494b9141d92b6d70f90593f16ccb208af9e91
Yoseph Getachew <Yoseph.GetachewW@ethio.local>
2024-10-28 22:37:41 +0300

    editing

--- src/main/java/com/example/demo/Controller/App.java
+++ src/main/java/com/example/demo/Controller/App.java
@@ -2,6 +2,7 @@ package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -15,7 +16,7 @@ public class App {
         return "login";
     }
     
-    @GetMapping("/apply")
+    @PostMapping("/applay")
     public String getMethodName(@RequestParam String param) {
         return "applicant";
     }
