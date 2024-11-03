b454fb0a4c647a325268a70b2dd27ab79dc04a8e
Yoseph Getachew <Yoseph.GetachewW@ethio.local>
2024-11-03 22:59:40 +0300

    add new login page

--- src/main/java/com/example/demo/Controller/App.java
+++ src/main/java/com/example/demo/Controller/App.java
@@ -3,7 +3,7 @@ package com.example.demo.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class App {
 
-    @GetMapping("/test")
+    @GetMapping("/")
     public String getTest() {
         return "login";
     }
